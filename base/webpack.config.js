77d834db6bae9340e46b2d422a2807ba142a7583
t.shibuta <make.app.ts@gmail.com>
2017-09-10 02:41:10 +0900

    基礎となるwebpackの設定を追加

---
+++ base/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin')
+
+
+module.exports = {
+  devtool: 'cheap-eval-source-map',
+  entry: {
+    app: path.join(__dirname, 'src', 'app'),
+  },
+  output: {
+    path: path.join(__dirname, 'dist', 'public'),
+    filename: '[name].[chunkhash].js',
+    publicPath: '/public/',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: path.join(__dirname, 'dist', 'index.html'),
+      template: path.join(__dirname, 'src', 'index.html'),
+      inject: true,
+    }),
+    new ScriptExtHtmlWebpackPlugin({
+      defaultAttribute: 'defer',
+      async: ['app'],
+    }),
+  ],
+}
