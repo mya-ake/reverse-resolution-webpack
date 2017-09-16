f612a1d90c79308dcd3a2151f5a16c6b34b24074
t.shibuta <make.app.ts@gmail.com>
2017-09-16 23:46:03 +0900

    追加

---
+++ babel/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin')
+
+
+module.exports = {
+  devtool: 'cheap-eval-source-map',
+  entry: {
+    app: path.join(__dirname, 'src', 'app'),
+  },
+  output: {
+    path: path.join(__dirname, 'dist', 'assets'),
+    filename: '[name].[chunkhash].js',
+    publicPath: '/assets/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: path.join(__dirname, 'dist', 'index.html'),
+      template: path.join(__dirname, 'src', 'index.html'),
+      inject: true,
+    }),
+    new ScriptExtHtmlWebpackPlugin({
+      defaultAttribute: 'defer',
+      async: ['app'],
+    }),
+  ],
+}
