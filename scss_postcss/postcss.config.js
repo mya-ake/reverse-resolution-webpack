41850ea686e012864a7b4411aba4ab9537b5e79a
t.shibuta <make.app.ts@gmail.com>
2017-09-18 01:01:27 +0900

    更新

--- scss_postcss/postcss.config.js
+++ scss_postcss/postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: [
     require('autoprefixer')({
-      browsers: ['IE 9', 'IE 10', 'IE 11', 'last 2 versions']
+      browsers: ['IE 11', 'last 2 versions']
     }),
   ],
 }
