3e2c705543bfd5c72e2b20d548bf8c7cef3cf113
t.shibuta <make.app.ts@gmail.com>
2017-09-21 01:51:23 +0900

    更新

--- vue/webpack.config.js
+++ vue/webpack.config.js
@@ -24,11 +24,15 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: "babel-loader"
+        use: [
+          'babel-loader',
+        ],
       },
       {
         test: /\.vue$/,
-        loader: 'vue-loader',
+        use: [
+          'vue-loader',
+        ],
       },
     ],
   },
